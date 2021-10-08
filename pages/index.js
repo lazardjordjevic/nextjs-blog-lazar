495f381cffe4bcbb21dfd2a4bf028f6232f89681
lazardjordjevic <lazar.djordjevic.it@gmail.com>
2021-10-08 14:56:24 +0200

    Added title and meta tags for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>NextJS Blog by Lazar</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1 className="page-title">Lazar Djordjevic</h1>
       <h2 className="page-subtitle">NextJS blog</h2>
     </>
