abc74d2a9550c1135d0d2470a1e2cb0e25817178
lazardjordjevic <lazar.djordjevic.it@gmail.com>
2021-10-08 14:59:56 +0200

    SEO performance improvement

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
       <Head>
         <title>NextJS Blog by Lazar</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Lazar Djordjevic lazardjordjevic blog nextjs" />
       </Head>
       <h1 className="page-title">Lazar Djordjevic</h1>
       <h2 className="page-subtitle">NextJS blog</h2>
