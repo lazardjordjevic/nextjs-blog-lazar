50861d4bda9784470de1a349d89c018c1bf9f9fb
lazardjordjevic <lazar.djordjevic.it@gmail.com>
2022-01-14 14:20:04 +0100

    Added MDX

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,55 @@
-import Head from 'next/head'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>NextJS Blog by Lazar</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Lazar Djordjevic lazardjordjevic blog nextjs" />
-      </Head>
-      <h1 className="page-title">Lazar Djordjevic</h1>
-      <h2 className="page-subtitle">NextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blogNextJS blog NextJS blog</h2>
-    </>
-  )
+const Home = ({ posts }) => {
+    return (
+        <div className='mt-5'>
+            {posts.map((post, index) => (
+                <Link href={'/' + post.slug} passHref key={index}>
+                    <div>
+                        <div className='row g-0'>
+                            <h5 className='card-title'>
+                                {post.frontMatter.title}
+                            </h5>
+                            <p className='card-text'>
+                                {post.frontMatter.description}
+                            </p>
+                            <p className='card-text'>
+                                <small className='text-muted'>
+                                    {post.frontMatter.date}
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
 }
+
+export const getStaticProps = async () => {
+    const files = fs.readdirSync(path.join('posts'))
+
+    const posts = files.map(filename => {
+        const markdownWithMeta = fs.readFileSync(
+            path.join('posts', filename),
+            'utf-8'
+        )
+        const { data: frontMatter } = matter(markdownWithMeta)
+
+        return {
+            frontMatter,
+            slug: filename.split('.')[0],
+        }
+    })
+
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
+export default Home
