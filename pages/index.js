825166432e0699dfb80312471ade7b66d04068aa
lazardjordjevic <lazar.djordjevic.it@gmail.com>
2022-03-14 20:55:55 +0100

    add arrow navigation, update pages

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,30 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import Link from 'next/link'
-
 import { applyPolyfills, defineCustomElements } from 'lazer-component/loader'
 import { useEffect } from 'react'
 import { useRouter } from 'next/dist/client/router'
+import Head from 'next/head'
 
 const Home = ({ posts }) => {
     const router = useRouter()
-    let test = null
+    let pageNumber = null
 
     const handleKeypress = e => {
-        if (test !== null) {
-            if (e.code === 'ArrowLeft' && test >= 0) {
-                test = test > 0 ? test - 1 : 0
-            } else if (e.code === 'ArrowRight' && test < posts.length - 1) {
-                test += 1
+        if (pageNumber !== null) {
+            if (e.code === 'ArrowLeft' && pageNumber >= 0) {
+                pageNumber = pageNumber > 0 ? pageNumber - 1 : 0
+            } else if (
+                e.code === 'ArrowRight' &&
+                pageNumber < posts.length - 1
+            ) {
+                pageNumber += 1
             }
         } else {
-            test = 0
+            pageNumber = 0
         }
 
-        // test = e.code === 'ArrowLeft' && test >= 0 ? test > 0 ? test - 1 : 0 : e.code === 'ArrowRight' && test < posts.length - 1 ? test += 1 : 0
-
-        router.push(`/${test}`)
+        router.push(`/${pageNumber}`)
     }
 
     useEffect(() => {
@@ -35,20 +36,29 @@ const Home = ({ posts }) => {
     })
 
     return (
-        <div className='mt-5'>
-            {posts.map((post, index) => (
-                <Link href={'/' + post.slug} passHref key={index}>
-                    <div className='post-card'>
-                        <h5 className='post-card__title'>
-                            {post.frontMatter.title}
-                        </h5>
-                        <span className='post-card__description'>
-                            {post.frontMatter.description}
-                        </span>
-                        <span className='post-card__date'>{post.frontMatter.date}</span>
-                    </div>
-                </Link>
-            ))}
+        <div>
+            <Head>
+                <link
+                    id='favicon'
+                    rel='shortcut icon'
+                    type='image/png'
+                    href='https://res.cloudinary.com/elazar93/image/upload/v1647269195/vega_logo_dage4b.png'
+                />
+                <title>Web Components </title>
+            </Head>
+            <div className='mt-5'>
+                {posts.map((post, index) => (
+                    <Link href={'/' + post.slug} passHref key={index}>
+                        <div className='lazer-card__wrapp'>
+                            <lazer-card
+                                card-title={post.frontMatter.title}
+                                card-description={post.frontMatter.description}
+                                card-date={post.frontMatter.date}
+                            ></lazer-card>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
