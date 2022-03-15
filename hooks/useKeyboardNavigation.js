a13b03204f9cc3ca7a4f9cecbfb50bd0bf4eb845
lazardjordjevic <lazar.djordjevic.it@gmail.com>
2022-03-15 11:59:07 +0100

    Addeed more slides

--- hooks/useKeyboardNavigation.js
+++ hooks/useKeyboardNavigation.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 export function useKeyboardNavigation() {
   const router = useRouter();
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(router.query?.slug ? Number(router.query?.slug) : 0);
 
   const nextPage = () => setPage((value) => (value += 1));
   const previousPage = () => setPage((value) => Math.max((value -= 1), 0));
@@ -11,6 +11,7 @@ export function useKeyboardNavigation() {
   const handleKeypress = (event) => {
     switch (event.code) {
       case "ArrowLeft":
+        console.log(router);
         return previousPage();
       case "ArrowRight":
         return nextPage();
